0beb026d6940e727ea8f904bb0bbdcae56d6390e
TarasKochubey <t.v.kochubey@gmail.com>
2026-05-11 21:04:00 +0200

    Initial Commit

---
+++ Solodko-iOS/Solodko_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Solodko_iOSApp.swift
+//  Solodko-iOS
+//
+//  Created by Taras Kochubey on 11.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct Solodko_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
